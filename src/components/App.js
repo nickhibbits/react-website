6b4be983f80b3bf4a140c05b5b1bc5cd732149f1
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-07-02 11:02:26 -0700

    mobile layout, colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import logo from "../logo.svg";
 import "../styles/App.css";
+import Nav from "./Nav";
 
 function App() {
-  return <div className="app-component"></div>;
+  return (
+    <div className="app-component">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
