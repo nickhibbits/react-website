29086820d8ae42facb9155df1acafb049985829b
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-07-02 14:49:08 -0700

    style navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,7 @@ import "../styles/Nav.css";
 function Nav() {
   return (
     <div className="nav-component">
+      <div className="N">NH</div>
       <div className="icon-wrapper">
         <HiOutlineMenuAlt3 size={25} color="white" />
       </div>
