162527ac6a5abf6fa836d4840dfe10950065d133
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-07-02 20:25:23 -0700

    nav transition

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,51 @@
+import { useState } from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
 import "../styles/Nav.css";
 
 function Nav() {
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+
+  // const class = navMenuOpen ? "menu-open" : "menu-closed"
+
+  function toggleNav(e) {
+    e.preventDefault();
+
+    setNavMenuOpen(!navMenuOpen);
+  }
+
+  if (navMenuOpen) {
+    return (
+      <div className="nav-component-menu-open">
+        <div className="navbar-top">
+          <div className="N">NH</div>
+          <div className="icon-wrapper" onClick={(e) => toggleNav(e)}>
+            <IoClose size={25} color="white" />
+          </div>
+        </div>
+        <div className="navbar-dropdown">
+          <Link className="dropdown-link" to="">
+            Home
+          </Link>
+          <Link className="dropdown-link" to="">
+            About
+          </Link>
+          <Link className="dropdown-link" to="">
+            Projects
+          </Link>
+          <Link className="dropdown-link" to="">
+            Connect
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="nav-component">
       <div className="N">NH</div>
-      <div className="icon-wrapper">
+      <div className="icon-wrapper" onClick={(e) => toggleNav(e)}>
         <HiOutlineMenuAlt3 size={25} color="white" />
       </div>
     </div>
