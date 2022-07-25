a667a096b38da1ca0abdc6d3ff32c6f04c4097ae
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-07-24 18:11:52 -0700

    add zoomies logo

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,12 @@
 import "../styles/Work.css";
+import { ReactComponent as ZoomiesLogo } from "../ZoomiesLogo.svg";
 
 function ProjectCard() {
   return (
     <div className="project-card-component">
-      <div className="project-image">image</div>
+      <div className="project-image-wrapper">
+        <ZoomiesLogo />
+      </div>
       <div className="project-description">description</div>
     </div>
   );
