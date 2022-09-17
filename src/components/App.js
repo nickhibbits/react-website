87e3d8d2029ffa1154fefc2796b697bd5c85d804
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-09-17 15:49:58 -0700

    first attempt layering grids

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import "../styles/App.scss";
+import Home from "./Home";
 
-import { MdOutlineConnectWithoutContact } from "react-icons/md";
-import { MdWorkOutline } from "react-icons/md";
-import { MdInfoOutline } from "react-icons/md";
+// import { MdOutlineConnectWithoutContact } from "react-icons/md";
+// import { MdWorkOutline } from "react-icons/md";
+// import { MdInfoOutline } from "react-icons/md";
 
 function App() {
   return (
     <main className="app-component">
-      <h1>Appe</h1>
-      <p>text</p>
-      <MdOutlineConnectWithoutContact />
-      <MdWorkOutline />
-      <MdInfoOutline />
+      <section className="home-container">
+        <div className="box-trees"> trees </div>
+        <div className="box-fade">fade</div>
+        <div className="home-wrapper">
+          <Home />
+        </div>
+      </section>
     </main>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home-component">
+      <section className="info-wrapper">
+        <h1>Hi, I'm Nick</h1>
+        <p>
+          Welcome to my website and personal playground. <br />
+          <br />
+          Built using React and SCSS, I use this space to showcase my work, try
+          new techniques, and refine my understanding of various concepts I come
+          across in my time spent as a web developer.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
