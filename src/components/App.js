9d04f599d4da630fb3cdb4d16c9d6f94769296e5
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-09-17 21:56:49 -0700

    home screen grid and styles-- revisit for mobile

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
 import "../styles/App.scss";
 import Home from "./Home";
 
-// import { MdOutlineConnectWithoutContact } from "react-icons/md";
-// import { MdWorkOutline } from "react-icons/md";
-// import { MdInfoOutline } from "react-icons/md";
+import trees from "../assets/images/trees-overhead.jpg";
+
+import { MdOutlineConnectWithoutContact } from "react-icons/md";
+import { MdWorkOutline } from "react-icons/md";
+import { MdInfoOutline } from "react-icons/md";
 
 function App() {
+  const size = 80;
   return (
     <main className="app-component">
       <section className="home-container">
-        <div className="box-trees"> trees </div>
-        <div className="box-fade">fade</div>
-        <div className="home-wrapper">
-          <Home />
+        <div className="box-fade">
+          <ul className="icon-nav">
+            <li>
+              <MdInfoOutline size={size} color={"white"} />
+            </li>
+            <li>
+              <MdWorkOutline size={size} color={"white"} />
+            </li>
+            <li>
+              <MdOutlineConnectWithoutContact size={size} color={"white"} />
+            </li>
+          </ul>
         </div>
+        <div className="box-trees">
+          <img src={trees} alt="overhead shot of forest" />
+        </div>
+        <Home />
       </section>
     </main>
   );
