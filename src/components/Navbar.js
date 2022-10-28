53b1e44ca592a8afc08f46113f44e102a5051dcd
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-10-28 16:54:15 -0500

    continue mobile nav refactor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,23 @@
 import { MdOutlineClose } from "react-icons/md";
 import { MdOutlineMenu } from "react-icons/md";
 import { useRef, useState } from "react";
+import { useWindowCheck } from "../customHooks";
 
 import "../styles/components/Nav.scss";
 import nickProfessional from "../assets/images/nick-professional.jpg";
 
 function Navbar() {
   const [displayMenuIcon, setDisplayMenuIcon] = useState(true);
-  const navOptionsWrapper = useRef();
+  const navOptionsContainer = useRef();
+  const width = useWindowCheck();
 
   function toggleNavOptions() {
-    let visible = navOptionsWrapper.current.getAttribute("data-visible");
+    let visible = navOptionsContainer.current.getAttribute("data-visible");
     if (visible === "false") {
-      navOptionsWrapper.current.setAttribute("data-visible", true);
+      navOptionsContainer.current.setAttribute("data-visible", true);
       setDisplayMenuIcon(false);
     } else if (visible === "true") {
-      navOptionsWrapper.current.setAttribute("data-visible", false);
+      navOptionsContainer.current.setAttribute("data-visible", false);
       setDisplayMenuIcon(true);
     }
   }
@@ -45,46 +47,56 @@ function Navbar() {
         </div>
 
         <div
-          ref={navOptionsWrapper}
-          className="nav-options-wrapper"
+          className="nav-options-mobile-container"
           data-visible={false}
+          ref={navOptionsContainer}
         >
-          <li>
-            <a
-              href="#home-container"
-              className="nav-link"
-              onClick={() => toggleNavOptions()}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#about-container"
-              className="nav-link"
-              onClick={() => toggleNavOptions()}
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="#work-container"
-              className="nav-link"
-              onClick={() => toggleNavOptions()}
-            >
-              Work
-            </a>
-          </li>
-          <li>
-            <a
-              href="#connect-container"
-              className="nav-link"
-              onClick={() => toggleNavOptions()}
-            >
-              Connect
-            </a>
-          </li>
+          <div className="nav-options-mobile-avatar-wrapper">
+            <img
+              src={nickProfessional}
+              alt="nick-professional"
+              className="nav-options-mobile-avatar"
+              style={width > 850 ? { display: "none" } : { display: "block" }}
+            />
+          </div>
+          <div className="nav-options-wrapper">
+            <li>
+              <a
+                href="#home-container"
+                className="nav-link"
+                onClick={() => toggleNavOptions()}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about-container"
+                className="nav-link"
+                onClick={() => toggleNavOptions()}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#work-container"
+                className="nav-link"
+                onClick={() => toggleNavOptions()}
+              >
+                Work
+              </a>
+            </li>
+            <li>
+              <a
+                href="#connect-container"
+                className="nav-link"
+                onClick={() => toggleNavOptions()}
+              >
+                Connect
+              </a>
+            </li>
+          </div>
         </div>
       </ul>
     </nav>
