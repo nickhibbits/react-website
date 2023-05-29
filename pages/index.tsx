84323be5021d7af321fc1a4c3a41226420cdb353
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-05-28 17:21:27 -0700

    sync color and image change animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,81 +20,115 @@ import Image from "next/image";
 import imageStyles from "$/styles/imageStyles/ImageTransitions.module.scss";
 
 function Home() {
-  const bgControls = useAnimation();
-  const imageControls = useAnimation();
-  const titleControls = useAnimation();
   const width = useWindowCheck();
 
-  // 3 animations -- fade change in: gradient bg color, images, titles
-  // all will be synced, and the animation will consist in a change in opacity
-  // 3 components get three useAnimate() hooks
-  // on mount useAnimate().start() all animations in unison
+  const opacitySequence = {
+    base: { opacity: [0, 1, 1, 1, 0, 1] },
+    sibling: { opacity: [0, 1, 1, 0, 0] },
+  };
+  const transition = (delay: number) => {
+    return { base: { duration: 5, delay }, sibling: { duration: 5, delay } };
+  };
 
-  return (
-    <div className={appStyles.app_container}>
-      <div className={layoutStyles.app_component}>
-        <section className={layoutStyles.home_container} id="home_container">
-          <div className={appStyles.intro_wrapper}>
-            {/* <Intro /> */}
-            <div className={width < 1150 ? `${layoutStyles.component}` : ""}>
-              <section className={layoutStyles.info_wrapper}>
-                <h1>Hi, I'm Nick</h1>
-                <p>Programmer • Sound Enthusiast • Forest Dweller</p>
-              </section>
+  if (width) {
+    return (
+      <div className={appStyles.app_container}>
+        <div className={layoutStyles.app_component}>
+          <section className={layoutStyles.home_container} id="home_container">
+            <div className={appStyles.intro_wrapper}>
+              {/* <Intro /> */}
+              <div className={width < 1150 ? `${layoutStyles.component}` : ""}>
+                <section className={layoutStyles.info_wrapper}>
+                  <h1>Hi, I'm Nick</h1>
+                  <p>Programmer • Sound Enthusiast • Forest Dweller</p>
+                </section>
+              </div>
             </div>
-          </div>
 
-          <div className={appStyles.color_fade_container}>
-            <div className={appStyles.color_fade_box} />
-            <div className={appStyles.color_fade_box} />
-            <div className={appStyles.color_fade_box} />
-            {/* <IconNav /> */}
-          </div>
+            <div className={appStyles.color_fade_container}>
+              <motion.div
+                animate={opacitySequence.base}
+                transition={transition(0).base}
+                className={appStyles.color_fade_box}
+              />
+              <motion.div
+                animate={opacitySequence.sibling}
+                transition={transition(1).sibling}
+                className={appStyles.color_fade_box}
+              />
+              <motion.div
+                animate={opacitySequence.sibling}
+                transition={transition(2).sibling}
+                className={appStyles.color_fade_box}
+              />
+              {/* <IconNav /> */}
+            </div>
 
-          {/* <ImageTransition /> */}
-          <div
-            className={imageStyles.image_transition_container}
-            style={{ position: "relative" }}
-          >
-            <Image
-              className={imageStyles.img}
-              src={treesImage}
-              alt="overhead forest shot"
-              fill={true}
-              style={{
-                objectFit: "cover",
-                transform: "scale(1.4)",
-                objectPosition: "right",
-              }}
-            />
-            <Image
-              className={imageStyles.img}
-              src={dialsImage}
-              alt="sound eq dials"
-              fill={true}
-              style={{
-                objectFit: "contain",
-                transform: "scale(3.7)",
-              }}
-            />
-            <Image
-              className={imageStyles.img}
-              src={codeImage}
-              alt="code"
-              fill={true}
-              style={{
-                objectFit: "cover",
-                transform: "scale(1.4)",
-              }}
-            />
-          </div>
-        </section>
-        <About />
-        <Work />
-        <Connect />
+            {/* <ImageTransition /> */}
+            <div
+              className={imageStyles.image_transition_container}
+              style={{ position: "relative" }}
+            >
+              <motion.div
+                animate={opacitySequence.base}
+                transition={transition(0).base}
+                className={imageStyles.image_wrapper}
+              >
+                <Image
+                  className={imageStyles.img}
+                  src={codeImage}
+                  alt="code"
+                  fill={true}
+                  style={{
+                    objectFit: "cover",
+                    transform: "scale(1.4)",
+                  }}
+                />
+              </motion.div>
+
+              <motion.div
+                animate={opacitySequence.sibling}
+                transition={transition(1).sibling}
+                className={imageStyles.image_wrapper}
+              >
+                <Image
+                  className={imageStyles.img}
+                  src={dialsImage}
+                  alt="sound eq dials"
+                  fill={true}
+                  style={{
+                    objectFit: "contain",
+                    transform: "scale(3.7)",
+                  }}
+                />
+              </motion.div>
+
+              <motion.div
+                animate={opacitySequence.sibling}
+                transition={transition(2).sibling}
+                className={imageStyles.image_wrapper}
+              >
+                <Image
+                  className={imageStyles.img}
+                  src={treesImage}
+                  alt="overhead forest shot"
+                  fill={true}
+                  style={{
+                    objectFit: "cover",
+                    transform: "scale(1.4)",
+                    objectPosition: "right",
+                  }}
+                />
+              </motion.div>
+            </div>
+          </section>
+          <About />
+          <Work />
+          <Connect />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
