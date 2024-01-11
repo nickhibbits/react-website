5772dd01df1e8c9089a695a2febd9854f0865401
Nicholas Hibbits <nicholashibbits@gmail.com>
2024-01-11 15:57:27 -0800

    make nav function on all screen sizes

--- components/Navbars/NavbarMobile/NavbarMobile.tsx
+++ components/Navbars/NavbarMobile/NavbarMobile.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { navOptions } from "$/constants/navOptions";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import navbarMobileStyles from "$/components/Navbars/NavbarMobile/NavbarMobile.module.scss";
 import layoutStyles from "$/styles/layout/layout.module.scss";
@@ -12,35 +12,46 @@ function NavbarMobile() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav
-      className={`${navbarMobileStyles.nav} ${layoutStyles.padding_wrapper}`}
-    >
-      <div
-        className={navbarMobileStyles.svgWrapper}
-        onClick={() => setMenuOpen(!menuOpen)}
+    <>
+      <nav
+        className={`${navbarMobileStyles.nav} ${layoutStyles.padding_wrapper}`}
       >
-        {menuOpen ? "Xsvg" : "menuSVG"}
-      </div>
+        <div
+          className={navbarMobileStyles.svgWrapper_menu}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          menuSVG
+        </div>
+      </nav>
+
       {menuOpen && (
-        <ul className={navbarMobileStyles.options_wrapper}>
-          {navOptions.map((option, i) => {
-            if (option !== "Resume") {
-              return (
-                <li className={navbarMobileStyles.option} key={i}>
-                  <a href={`#${option.toLowerCase()}`}>{option}</a>
-                </li>
-              );
-            } else {
-              return (
-                <li className={navbarMobileStyles.option} key={i}>
-                  <Link href={option.toLowerCase()}>{option}</Link>
-                </li>
-              );
-            }
-          })}
-        </ul>
+        <div className={navbarMobileStyles.options_container}>
+          <div
+            className={`${navbarMobileStyles.svgWrapper_x} ${layoutStyles.padding_wrapper}`}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            X
+          </div>
+          <ul className={navbarMobileStyles.options_wrapper}>
+            {navOptions.map((option, i) => {
+              if (option !== "Resume") {
+                return (
+                  <li className={navbarMobileStyles.option} key={i}>
+                    <a href={`#${option.toLowerCase()}`}>{option}</a>
+                  </li>
+                );
+              } else {
+                return (
+                  <li className={navbarMobileStyles.option} key={i}>
+                    <Link href={option.toLowerCase()}>{option}</Link>
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        </div>
       )}
-    </nav>
+    </>
   );
 }
 
