07c234ef4e2b2988f2d5d543b81784b3fd9f81a1
Nicholas Hibbits <nicholashibbits@gmail.com>
2024-01-11 14:31:39 -0800

    dev navbar for larger screen sizes

--- components/Navbars/Navbar/Navbar.tsx
+++ components/Navbars/Navbar/Navbar.tsx
@@ -1,7 +1,21 @@
 import navbarStyles from "$/components/Navbars/Navbar/Navbar.module.scss";
+import layoutStyles from "$/styles/layout/layout.module.scss";
+import { navOptions } from "$/constants/navOptions";
 
 function Navbar() {
-  return <nav className={navbarStyles.nav}>Navbar</nav>;
+  return (
+    <nav className={`${navbarStyles.nav} ${layoutStyles.component}`}>
+      <ul className={navbarStyles.options_wrapper}>
+        {navOptions.map((option, i) => {
+          return (
+            <li className={navbarStyles.option} key={i}>
+              {option}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
--- components/Navbars/NavbarMobile/NavbarMobile.tsx
+++ components/Navbars/NavbarMobile/NavbarMobile.tsx
@@ -1,4 +1,5 @@
 import navbarMobileStyles from "$/components/Navbars/NavbarMobile/NavbarMobile.module.scss";
+import { navOptions } from "$/constants/navOptions";
 
 function NavbarMobile() {
   return <nav className={navbarMobileStyles.nav}>NavbarMobile</nav>;
